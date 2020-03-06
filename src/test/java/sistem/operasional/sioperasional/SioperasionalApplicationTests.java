66373cfdc2295e9bf4a7bbff9e239ebbec1e5fbf
yudhapradiptar <50763128+yudhapradiptar@users.noreply.github.com>
2020-03-06 18:49:25 +0700

    first commit

---
+++ src/test/java/sistem/operasional/sioperasional/SioperasionalApplicationTests.java
@@ -0,0 +1,13 @@
+package sistem.operasional.sioperasional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SioperasionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
