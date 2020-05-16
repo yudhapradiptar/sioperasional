c915042a45de66421ecc0b8361906b0dcb1136d5
yudhapradiptar <50763128+yudhapradiptar@users.noreply.github.com>
2020-05-16 22:00:18 +0700

    fix: itemController

--- src/main/java/sistem/operasional/sioperasional/controller/ItemController.java
+++ src/main/java/sistem/operasional/sioperasional/controller/ItemController.java
@@ -112,7 +112,7 @@ public class ItemController {
     }
 
     @RequestMapping(value = "/update/{idItem}", method = RequestMethod.GET)
-    public String UpdateStatusItem(@PathVariable("idItem") String idItem, Model model){
+    public String UpdateStatusItem(@PathVariable("idItem") String idItem, @AuthenticationPrincipal UserDetails currentUser, Model model){
         //ItemModel item = itemService.getItemDetailByIdItem(Long.parseLong(idItem));
         List<StatusItemModel> statusItem = statusItemService.getListStatusItem();
         //ItemModel itemDetail = item;
